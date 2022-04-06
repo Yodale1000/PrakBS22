bf41732c9027a77bae856809a2e87e0952e9c12a
Michel <yodale1000@live.de>
2022-04-06 20:07:53 +0200

    Initial commit

---
+++ cmake-build-docker/CMakeFiles/PrakBS22.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PrakBS22.dir/main.c.o"
+  "PrakBS22.pdb"
+  "PrakBS22"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PrakBS22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
